a305e7108e518437c925110278c26f198c2983b1
dangthuanvo <20520314@gm.uit.edu.vn>
2025-08-03 17:02:48 +0700

    modify login page

---
+++ Frontend/src/app/components/animated-background/animated-background.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animated-background',
+  templateUrl: './animated-background.component.html',
+  styleUrls: ['./animated-background.component.scss']
+})
+export class AnimatedBackgroundComponent {}
